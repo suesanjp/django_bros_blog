a850f55d5d6cd5f4d43a4e21eb4305b78c838a28
suesanjp <a.drop.of.my.essence@gmail.com>
2021-02-04 19:42:33 +0900

    created list view on index

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+
+
+def index(request):
+    blogs = Blog.objects.order_by('-created_datetime')
+    return render(request, 'blogs/index.html', {'blogs': blogs})
+
+# class ModelDetailView(DetailView):
+#     model = Model
+#     template_name = ".html"
+
+
+# class ModelListView(ListView):
+#     model = Model
+#     template_name = ".html"
